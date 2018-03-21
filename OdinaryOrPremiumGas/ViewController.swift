7a34a727d65216664df461a8d99c3e246fc79023
eleves <eleves@raphael.local>
2018-03-21 19:57:21 -0400

    v1.0

--- OdinaryOrPremiumGas/ViewController.swift
+++ OdinaryOrPremiumGas/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ordinaryField: UITextField!
+    @IBOutlet weak var premiumField: UITextField!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
